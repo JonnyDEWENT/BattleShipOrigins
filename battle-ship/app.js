20131bd21ad8d693103bcb6c134928d516cd2500
JonnyDEWENT <32647918+JonnyDEWENT@users.noreply.github.com>
2018-11-16 18:37:00 -0500

    Add files via upload

---
+++ battle-ship/app.js
@@ -0,0 +1,90 @@
+
+var battleship;
+
+
+
+function authenticate(ship){
+    battleship = ship;
+    const txtEmail = document.getElementById("email");
+    const txtPassword = document.getElementById("password");
+    const btnLogin = document.getElementById("btnLogIn");
+    const btnSignup = document.getElementById("btnSignUp");
+    const btnSignout = document.getElementById("btnLogOut");
+    
+
+    //Add login 
+    btnLogin.addEventListener('click', e=>{
+        const email = txtEmail.value;
+        const password = txtPassword.value;
+
+        const auth = firebase.auth();
+
+        const promise = auth.signInWithEmailAndPassword(email,password);
+        
+
+        promise
+        .then(() => {
+            switchToLobby(email);
+        })
+        .catch(e => console.log(e.message));
+
+    });
+
+    btnSignout.addEventListener('click', e => {
+        firebase.auth().signOut();
+
+    });
+
+    
+    btnSignup.addEventListener('click', e=> {
+        const email = txtEmail.value;
+        const password = txtPassword.value;
+
+        const auth = firebase.auth();
+
+        const promise = auth.createUserWithEmailAndPassword(email,password);
+        
+
+        promise
+        .then(() => {
+            switchToLobby(email);
+        })
+        .catch(e => console.log(e.message));
+    });
+
+        firebase.auth().onAuthStateChanged(firebaseUser => {
+            if(firebaseUser){
+                console.log(firebaseUser);
+                btnSignout.classList.remove('hide');
+            }
+            else {
+                console.log('not logged in');
+            }
+        });
+   
+
+}
+
+function switchToLobby(email){
+    battleship.login = 1;
+    battleship.username = email;
+    const btnNewGame=document.getElementById("btnNewGame");
+    btnNewGame.addEventListener('click', startNewGame());
+}
+
+
+function startNewGame(){
+    playerOne = battleship.username;
+    playerTwo = "";
+    boardOne = [];
+    boardTwo = [];
+    messageOne = "";
+    messageTwo = "";
+    status = "waiting";
+    winner = "";
+
+    let newGame = gameRef.push();
+    newGame.set({playerOne: playerOne, playerTwo: playerTwo, boardOne: boardOne, boardTwo: boardTwo, messageOne: messageOne, messageTwo: messageTwo,
+                    status: status, winner: winner});
+    
+}
