ed5b3060e313f430e9af7fa8d8bf9a1327fad176
JonnyDEWENT <32647918+JonnyDEWENT@users.noreply.github.com>
2018-11-16 23:07:34 -0500

    Added 10X10 array being added to db when new game started

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-function authenticate(){
+
+var battleship;
+
+
+
+function authenticate(ship){
+    battleship = ship;
     const txtEmail = document.getElementById("email");
     const txtPassword = document.getElementById("password");
     const btnLogin = document.getElementById("btnLogIn");
     const btnSignup = document.getElementById("btnSignUp");
     const btnSignout = document.getElementById("btnLogOut");
+    
 
     //Add login 
     btnLogin.addEventListener('click', e=>{
@@ -13,8 +20,13 @@ function authenticate(){
         const auth = firebase.auth();
 
         const promise = auth.signInWithEmailAndPassword(email,password);
+        
 
-        promise.catch(e => console.log(e.message));
+        promise
+        .then(() => {
+            switchToLobby(email);
+        })
+        .catch(e => console.log(e.message));
 
     });
 
@@ -23,6 +35,7 @@ function authenticate(){
 
     });
 
+    
     btnSignup.addEventListener('click', e=> {
         const email = txtEmail.value;
         const password = txtPassword.value;
@@ -30,8 +43,12 @@ function authenticate(){
         const auth = firebase.auth();
 
         const promise = auth.createUserWithEmailAndPassword(email,password);
+        
 
         promise
+        .then(() => {
+            switchToLobby(email);
+        })
         .catch(e => console.log(e.message));
     });
 
@@ -46,4 +63,38 @@ function authenticate(){
         });
    
 
+}
+
+function switchToLobby(email){
+    battleship.login = 1;
+    battleship.username = email;
+    const btnNewGame=document.getElementById("btnNewGame");
+    btnNewGame.addEventListener('click', startNewGame());
+}
+
+function createEmptyBoard(){
+    var board = [];
+    for (var x = 0; x < 10; x++){
+        board[x] = [0,0,0,0,0,0,0,0,0,0];
+    }
+    
+    return board;
+
+}
+
+
+function startNewGame(){
+    playerOne = battleship.username;
+    playerTwo = "";
+    boardOne = createEmptyBoard();
+    boardTwo = createEmptyBoard();
+    messageOne = "";
+    messageTwo = "";
+    status = 1;
+    winner = "";
+
+    let newGame = gameRef.push();
+    newGame.set({playerOne: playerOne, playerTwo: playerTwo, boardOne: boardOne, boardTwo: boardTwo, messageOne: messageOne, messageTwo: messageTwo,
+                    status: status, winner: winner});
+    
 }
